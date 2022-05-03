6a1d9be81e8bb9fc427f4e80249991ad82a54c7d
Shruti Agarwal <shruagarwal01@gmail.com>
2022-05-03 17:30:43 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# AUTOMATED SKETCH BOT 🤖
+
+- I have built a *Twitter Bot* which automatically generates a pencil sketch of an image of your choice when tweeted using **#PicSketch**.
+<p align="center"> Cool right!😎</p>
+
+## Demo below --->
+
+![alt text]()
+
+
+## Try it out in simple steps : 👇
+1. Go to your Twitter Account
+2. Click on the *Tweet* button / *feather icon* on Mobile
+3. Use #️⃣*PicSketch* and add an image (your choice)
+4. Click on Tweet!
+5. Refresh your Twitter Home feed and within seconds the bot will notify you with a pencil sketch of the image!
+
+**For more info, please check my post on Twitter :** *https://twitter.com/Shru_explores/status/1497897213292257280*
+
+### Try out the Bot now as it is LIVE!!  
+
+- This Bot is hosted over : ![Heroku](https://img.shields.io/badge/heroku-%23430098.svg?style=for-the-badge&logo=heroku&logoColor=white)
---
+++ bot.py
@@ -0,0 +1,88 @@
+# Imports --
+import os
+from env_load import load_twitter_env
+from io import BytesIO
+from time import sleep
+import requests
+import tweepy
+import cv2
+from PIL import Image
+from PIL import ImageFile
+import numpy as np
+
+# PIL.ImageFile.ImageFile Class Constant -- Whether or not to load truncated image files
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+# Loading twitter credentials
+consumer_key, consumer_secret, access_token, access_token_secret = load_twitter_env()
+
+# Authenticate to Twitter using Tweepy
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Connect to the TWITTER API
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+
+# This function opens the image using PIL library & then transforms it into a pencil sketch using 'sketch' function below.
+# After that, it uploads the sketch image in reply to the tweet posted earlier.
+def tweet_image(image, username, status_id):
+    if image.status_code == 200:
+        i = Image.open(BytesIO(image.content))
+        sketch(i)
+        response = api.media_upload('images/sketch.jpg')
+        api.update_status(status='@{0}'.format(username), in_reply_to_status_id=status_id, media_ids=[response.media_id])
+    else:
+        print("image app fail!")
+
+
+# Below functions help transform the image into a pencil sketch using cv2 library
+# The new image is being converted back into a PIL Image.
+def dodge(x, y):
+  return cv2.divide(x,  255 - y, scale=256)
+
+def sketch(i):
+  pil_image = i.convert('RGB')
+  open_cv_image = np.array(pil_image)
+
+  # Convert RGB to BGR
+  open_cv_image = open_cv_image[:, :, ::-1].copy()
+  img_gray = cv2.cvtColor(open_cv_image, cv2.COLOR_BGR2GRAY)
+  img_invert = cv2.bitwise_not(img_gray)
+  img_smoothing = cv2.GaussianBlur(img_invert, (21, 21), sigmaX=0, sigmaY=0)
+  final_img = dodge(img_gray, img_smoothing)
+
+  pil_image = Image.fromarray(final_img)
+  return pil_image.save('images/sketch.jpg')
+
+
+# BotStreamer class handles -- checking for a tweet with an image, fetching the link of image &
+# applying tweet_image() function to tweet the reply with the new (sketch) image.
+class BotStreamer(tweepy.StreamListener):
+
+  # Called when a new status arrives which is passed down from the on_data method of the StreamListener
+  def on_status(self, status):
+    username = status.user.screen_name
+    status_id = status.id
+
+    # Checks if there is any media-entity
+    for media in status.entities.get("media",[{}]):
+        try:
+            # Checks if the entity is of the type "photo"
+            if media.get("type",None) == "photo":
+                image_content=requests.get(media["media_url"], stream=True)
+
+            tweet_image(image_content, username, status_id)
+            sleep(30)
+        except tweepy.TweepError as e:
+            print(e.reason)
+        except StopIteration:
+            break
+
+
+# Creating Stream object
+myStreamListener = BotStreamer()
+stream = tweepy.Stream(api.auth, myStreamListener)
+
+# Filters real-time tweets which uses '#PicSketch' keyword
+stream.filter(track=["PicSketch"])
