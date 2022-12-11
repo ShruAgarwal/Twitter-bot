470617237db53bb102b78d5963cec39ce5a691f6
Shruti Agarwal <82811717+ShruAgarwal@users.noreply.github.com>
2022-12-11 19:15:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,10 +13,10 @@ import numpy as np
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 # Loading twitter credentials
-API_KEY = os.environ("API_KEY")
-API_SECRET = os.environ("API_SECRET")
-ACCESS_TOKEN = os.environ("ACCESS_TOKEN")
-ACCESS_TOKEN_SECRET = os.environ("ACCESS_TOKEN_SECRET")
+API_KEY = os.getenv("API_KEY")
+API_SECRET = os.getenv("API_SECRET")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
 
 # Authenticate to Twitter using Tweepy
 auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
